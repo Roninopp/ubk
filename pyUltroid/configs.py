0db89a8edcc161853d44018f9423303cbc72ff3f
VIVEK <99870677+Roninopp@users.noreply.github.com>
2022-09-29 20:37:46 +0530

    Update configs.py

--- pyUltroid/configs.py
+++ pyUltroid/configs.py
@@ -37,19 +37,19 @@ class Var:
         sys.argv[5] if len(sys.argv) > 5 else config("REDIS_PASSWORD", default="dplEdSkGkGs0WCs7J2XdGRSLgQL0HHGT")
     )
     # extras
-    BOT_TOKEN = config("BOT_TOKEN", default=None)
-    LOG_CHANNEL = config("LOG_CHANNEL", default=0, cast=int)
-    HEROKU_APP_NAME = config("HEROKU_APP_NAME", default=None)
+    BOT_TOKEN = config("BOT_TOKEN", default="5420641611:AAFRadnL6Z16KVBIW72IyPl3V9OJ98YlmwI")
+    LOG_CHANNEL = config("LOG_CHANNEL", default=-1001754175512, cast=int)
+    HEROKU_APP_NAME = config("HEROKU_APP_NAME", default="ok")
     HEROKU_API = config("HEROKU_API", default=None)
-    VC_SESSION = config("VC_SESSION", default=None)
+    VC_SESSION = config("VC_SESSION", default="1BVtsOJUBu3_kEu_mV76-5FIKSgdBPlFXvTma7Fxyv96szkmXAkG6WT5Yi_P3sXZ11aVSvqFAAQZ5jCa9B-Mlb5uE3dustOUNgzjHRNFcTBxkMGfNZ7iD0kWIX8sTMrOOhD5wnUllCs3bpVxLWxxSiZfvVg66gq_ytKgk7MMw428Q9LN0DksGJPX3I3GZlvsFd1CKOFP-appcToR_nAAahsOtbB7aIu4IhbY8RYTo79k_m041xnGohdVgV9MkozgqGldx6osIIcUAji5t33AWB3o-ac6xDP5eLrTDRk3vCbkWo3F2W2QuO3z7Ex5cSqibgFYt7h1E_oDjgHFRoMqoL2TzsAvNsL8=")
     ADDONS = config("ADDONS", default=False, cast=bool)
     VCBOT = config("VCBOT", default=False, cast=bool)
     # for railway
-    REDISPASSWORD = config("REDISPASSWORD", default=None)
+    REDISPASSWORD = config("REDISPASSWORD", default="dplEdSkGkGs0WCs7J2XdGRSLgQL0HHGT")
     REDISHOST = config("REDISHOST", default=None)
     REDISPORT = config("REDISPORT", default=None)
     REDISUSER = config("REDISUSER", default=None)
     # for sql
-    DATABASE_URL = config("DATABASE_URL", default=None)
+    DATABASE_URL = config("DATABASE_URL", default="None)
     # for MONGODB users
-    MONGO_URI = config("MONGO_URI", default=None)
+    MONGO_URI = config("MONGO_URI", default="mongodb+srv://DARKAMAN:DARKAMAN@cluster0.snqhn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
